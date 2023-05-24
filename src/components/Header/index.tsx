46e0ccdf78ec1e1685b33a72c25bec15c6d083d3
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-24 14:31:25 -0300

    Header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,23 @@
-import { HeaderContainer } from './styles'
+import logo from '../../assets/Logo.svg'
+import { HeaderContainer, HeaderNav, LocationBtn, CartBtn } from './styles'
+
+import { MapPin, ShoppingCart } from 'phosphor-react'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <h1>HEADER</h1>
+      <img src={logo} alt="" />
+
+      <HeaderNav>
+        <LocationBtn>
+          <MapPin size={22} weight="fill" />
+          <span>Fortaleza - CE</span>
+        </LocationBtn>
+        <CartBtn>
+          {/* {cartQuantity >= 1 && <span>{cartQuantity}</span>} */}
+          <ShoppingCart size={22} weight="fill" />
+        </CartBtn>
+      </HeaderNav>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,11 +1,90 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
-  width: 100%;
-  background: red;
-  padding: 2rem;
-
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  padding: 2rem 0;
+
+  background: ${(props) => props.theme.background};
+
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 5;
+`
+
+export const HeaderNav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  > button {
+    display: flex;
+    align-items: center;
+
+    padding: 0.5rem;
+    border: none;
+    border-radius: 6px;
+
+    cursor: pointer;
+
+    transition: background 0.2s;
+  }
+`
+
+export const LocationBtn = styled.button`
+  gap: 0.25rem;
+
+  background: ${(props) => props.theme['purple-light']};
+
+  > span {
+    color: ${(props) => props.theme['purple-dark']};
+    font-size: 0.875rem;
+  }
+
+  > svg {
+    color: ${(props) => props.theme['purple-dark']};
+  }
+
+  &:hover {
+    background: ${(props) => props.theme['purple-dark']};
+
+    > span {
+      color: ${(props) => props.theme['purple-light']};
+      font-size: 0.875rem;
+    }
+
+    > svg {
+      color: ${(props) => props.theme['purple-light']};
+    }
+  }
+`
+
+export const CartBtn = styled.button`
+  background-color: ${(props) => props.theme['yellow-light']};
+  color: ${(props) => props.theme['yellow-dark']};
+
+  > span {
+    position: absolute;
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: 50%;
+    top: calc(2.7rem / 2);
+    right: calc(-1rem / 2);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 0.75rem;
+    font-weight: 700;
+    color: ${(props) => props.theme.white};
+    background: ${(props) => props.theme['yellow-dark']};
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme['yellow-dark']};
+    color: ${(props) => props.theme['yellow-light']};
+  }
 `
