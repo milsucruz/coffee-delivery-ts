304a51d53c24dc1732893a3534b471a3eed64759
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-24 09:48:39 -0300

    Global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    -webkit-font-smoothing: antialiased;
+    background: ${(props) => props.theme.background};
+    padding-bottom: 2rem;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
