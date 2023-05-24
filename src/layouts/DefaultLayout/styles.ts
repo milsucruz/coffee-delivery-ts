5ac04c8d75e67e13925666fc3f69e9fb5798e005
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-24 09:57:59 -0300

    Default Layout

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 70rem;
+  max-height: calc(100vh -10rem);
+  margin: 0rem auto;
+
+  display: flex;
+  flex-direction: column;
+`
