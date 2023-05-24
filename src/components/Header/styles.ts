142e180c5a3760213e25a23049e8dd77e738f643
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-24 09:55:42 -0300

    Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  background: red;
+  padding: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
