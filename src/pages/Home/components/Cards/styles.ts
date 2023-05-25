bd7b4cda2be4ab14f171eeef632b124eda0e0a66
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-25 15:47:30 -0300

    Card style fix

--- src/pages/Home/components/Cards/styles.ts
+++ src/pages/Home/components/Cards/styles.ts
@@ -69,7 +69,7 @@ export const CardShopping = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 1.5rem;
+  gap: 1rem;
 
   > p {
     color: ${(props) => props.theme['base-text']};
